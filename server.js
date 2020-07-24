120c228f8c5eb18becb0aedc8e66c2ddbdeb4f40
TommmyKelly <tommy_kelly@icloud.com>
2020-07-24 16:28:47 +0100

    new3

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ if (process.env.NODE_ENV !== 'production'){
 }
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.port || 3000;
 
 const express = require('express')
 const { request } = require('http')
